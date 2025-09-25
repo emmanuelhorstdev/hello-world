a023b4e411b2407f7987883f080813866d3b105d
emmanuelhorstdev <emmanuel.marinhostuart@gmail.com>
2025-09-25 11:25:38 -0300

    Adicionado system com java, interação com usuario

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args)  {
 
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá! Para cadastro na plataforma, por favor, digite seu nome COMPLETO:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Agora seu CPF (Cadastro de Pessoa Física):");
+        String cpf = scanner.nextLine();
+
+        System.out.println("Agora sua Idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Agora sua altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Agora seu peso:");
+        double peso = scanner.nextDouble();
+
+        scanner.nextLine();
+        System.out.println("E por último, digite seu estado civil:");
+        String estadoCivil = scanner.nextLine();;
+
 
     }
 
